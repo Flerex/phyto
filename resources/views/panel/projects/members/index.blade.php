5a51adabfb379899c9b067c809a65b7fbf53150a
Flerex <flerex@icloud.com>
2020-05-30 17:46:06 +0200

    Updated UI of project tables

--- resources/views/panel/projects/members/index.blade.php
+++ resources/views/panel/projects/members/index.blade.php
@@ -9,51 +9,49 @@
         </a>
     </div>
     @if(count($members))
-        <div class="box">
-            <table class="table is-fullwidth">
-                <thead>
-                <th>@lang('labels.name')</th>
-                <th>@lang('labels.status')</th>
-                <th>@lang('panel.projects.members.added_on')</th>
-                <th class="has-text-right">@lang('general.actions')</th>
-                </thead>
+        <table class="table is-fullwidth is-boxed">
+            <thead>
+            <th>@lang('labels.name')</th>
+            <th>@lang('labels.status')</th>
+            <th>@lang('panel.projects.members.added_on')</th>
+            <th class="has-text-right">@lang('general.actions')</th>
+            </thead>
 
-                <tbody>
-                @foreach($members as $member)
-                    <tr>
-                        <td>{{ $member->name }}</td>
-                        <td>
+            <tbody>
+            @foreach($members as $member)
+                <tr>
+                    <td>{{ $member->name }}</td>
+                    <td>
+                        @if($member->pivot->active)
+                            @lang('general.active')
+                        @else
+                            @lang('general.disabled')
+                        @endif
+                    </td>
+                    <td>{{ $member->pivot->created_at->diffForHumans() }}</td>
+                    <td class="has-text-right">
+                        <form
+                            action="{{ route('panel.projects.members.change_status', compact('project', 'member')) }}"
+                            method="POST">
+                            <input type="hidden" name="active" value="{{ (int) !$member->pivot->active }}">
                             @if($member->pivot->active)
-                                @lang('general.active')
+                                <button type="submit" class="button is-rounded is-light is-danger is-small"
+                                        title="{{ trans('panel.projects.members.disable') }}">
+                                    <span class="icon"><i class="fas fa-user-minus"></i></span>
+                                </button>
                             @else
-                                @lang('general.disabled')
+                                <button type="submit" class="button is-rounded is-light is-small"
+                                        title="{{ trans('panel.projects.members.enable') }}">
+                                    <span class="icon"><i class="fas fa-user-plus"></i></span>
+                                </button>
                             @endif
-                        </td>
-                        <td>{{ $member->pivot->created_at->diffForHumans() }}</td>
-                        <td class="has-text-right">
-                            <form
-                                action="{{ route('panel.projects.members.change_status', compact('project', 'member')) }}"
-                                method="POST">
-                                <input type="hidden" name="active" value="{{ (int) !$member->pivot->active }}">
-                                @if($member->pivot->active)
-                                    <button type="submit" class="button is-rounded is-light is-danger is-small"
-                                            title="{{ trans('panel.projects.members.disable') }}">
-                                        <span class="icon"><i class="fas fa-user-minus"></i></span>
-                                    </button>
-                                @else
-                                    <button type="submit" class="button is-rounded is-light is-small"
-                                            title="{{ trans('panel.projects.members.enable') }}">
-                                        <span class="icon"><i class="fas fa-user-plus"></i></span>
-                                    </button>
-                                @endif
-                                @csrf
-                            </form>
-                        </td>
-                    </tr>
-                @endforeach
-                </tbody>
-            </table>
-        </div>
+                            @csrf
+                        </form>
+                    </td>
+                </tr>
+            @endforeach
+            </tbody>
+        </table>
     @else
         <div class="message is-info">
             <div class="message-body">@lang('panel.projects.members.empty')</div>
